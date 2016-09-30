c73f05c80e7841e059f908c625bb2e2ad930231d
vigneshwar as <vigneshwar4028@gmail.com>
2016-09-30 12:46:58 +0530

    del

--- loops.py
+++
@@ -1,3 +0,0 @@
-n=int(raw_input())   #reading input from user
-for i in range(abs(n)):   #loop for iterating the values from 1 to n
-    print abs(i)**2   #printing the square of the values of the iterating value
