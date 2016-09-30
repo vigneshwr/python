10d4f0978de88d69729151b836c37b73f20d0946
vigneshwar as <vigneshwar4028@gmail.com>
2016-09-30 12:45:13 +0530

    del

--- arithmetic.py
+++
@@ -1,5 +0,0 @@
-x=int(raw_input())
-y=int(raw_input()) #reading two inputs from the user
-print x+y  #printing addition value
-print x-y  #printing subracted value
-print x*y  #printing product value
