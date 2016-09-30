35f383c6cffb8fb6525426d3c3af6b27a4766131
vigneshwar as <vigneshwar4028@gmail.com>
2016-09-30 12:45:32 +0530

    del

--- division.py
+++
@@ -1,4 +0,0 @@
-x=int(raw_input())
-y=int(raw_input())   #reading two inputs from the user
-print x//y   #performing integer division and printing
-print float(x)/float(y)    #performing floating division and printing 
